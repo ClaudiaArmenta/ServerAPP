3701a3cd0cc220de7434636cb68cc9d9e54d62df
AriadnaHCTec <65170456+AriadnaHCTec@users.noreply.github.com>
2021-09-29 19:02:35 -0500

    cambio en base de datos - Controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,152 +1,59 @@
 //Traer el modelo asociado a la tabla usuario
-//const Usuario = require('../models/usuario');
 const Sequelize = require('sequelize');
 const sequelize = require("../util/database");
-const Usuario = sequelize.models.User;
-const UsuarioData = sequelize.models.UserData;
+const User = sequelize.models.User;
 const path = require('path');
+const { RequestError } = require('tedious');
 
-/*exports.getHome = (req,res)=>{    
-    res.sendFile(path.join(__dirname,'..','views','home.html'));
-}
-
-exports.getAgregarUsuario = (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','registro.html'));
-}
-
-exports.getAgregarEncuesta = (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','registroFinal.html'));
-}
-
-exports.getInicioSesion = (req,res)=>{
-    //res.sendFile(path.join(__dirname,'..','views','inicioSesion.html'));
-    res.render('inicioSesion.html',{
-        error: 0
-    });
-}
-
-exports.getError = (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','error.html'));
-}*/
-
-exports.getAgregarUsuario = (req,res)=>{    
-    Usuario.create({
-        registrationDate : "2002-12-10"
-    }).then(resultado=>res.send(resultado))
+exports.postAgregarUsuario = (req,res)=>{    
+    console.log(req.body);
+    User.create({
+        email: req.body.email,
+        name: req.body.name,
+        password: req.body.password,
+        salt: req.body.salt,
+        dateOfBirth: req.body.birthday,
+        hasMonthlyDonation: req.body.hasMonthlyDonation,
+        confirmacionCorreo: req.body.confirmacionCorreo,
+    }).then(resultado=>console.log("Registro UserData exitoso"))
       .catch(error=>console.log(error));
+    res.json({ username: 'UwU' })
 };
+
+
+exports.postVerificaUsuario = (req,res)=>{    
+  console.log(req.body);
+  User.findByPk(req.body.email)
+  .then(resultado=>{
+      if(resultado){
+        res.json({ answer: 'NO' });  
+      }else{
+        res.json({ answer: 'SI' });
+      }        
+  })
+  .catch(error=>{
+      console.log(error);
+      res.send(error);
+  })
+};
+
 
 exports.postIniciarSesion = (req,res)=>{
-    console.log(req.body);
-    Usuario.findByPk(req.body.email)
-    .then(resultado=>{
-        if(resultado){
-            if(req.body.password == resultado.password){
-                res.send("osiosi");
-            }else{
-                res.send("incorrecto");
-            }
-        }else{
-            res.send("no existe el usuario");
-        }        
-    })
-    .catch(error=>{
-        console.log(error);
-        res.send(error);
-    })
+  console.log(req.body);
+  User.findByPk(req.body.email)
+  .then(resultado=>{
+      if(resultado){
+          if(req.body.password == resultado.password){                
+              res.send("siiuuuu");              
+          }else{
+              res.send("contraseña mala");
+          }
+      }else{
+          res.send("ni existe el usuario chavo");
+      }        
+  })
+  .catch(error=>{
+      console.log(error);
+      res.send(error);
+  })
 };
-
-/*exports.getConfirmacion = (req,res)=>{
-    //res.send("Registro exitoso");
-    res.sendFile(path.join(__dirname,'..','views','confirmacion.html'));
-};*/
-
-/*exports.getRegistros = (req,res)=>{
-    //Simular consulta
-    //query
-    //SELECT * from usuario;
-    Usuario.findAll()
-        .then(registros=>{           
-            var data=[];
-            registros.forEach(registro=>{
-                data.push(registro.dataValues);
-            });
-            console.log(data);
-            res.render('DataTable.html',{
-                personas:data,
-                sesion:"Autorizado",
-                hora:"14:00"
-            });
-        })    
-};*/
-
-
-/*exports.getHomeSteam = (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','views','homeSteam.html')); 
-}*/
-
-
-/*exports.postIniciarSesionPagina = (req,res)=>{
-    console.log(req.body);
-    Usuario.findByPk(req.body.nombreUsuario)
-    .then(resultado=>{
-        if(resultado){
-            if(req.body.contraseñaUsuario == resultado.contraseña){                
-                Usuario.findByPk(req.body.nombreUsuario)
-                .then(resultado=>{
-                    var data=[];
-                    data.push(resultado.dataValues);
-                    res.render('DataTable.html',{
-                        personas:data,
-                        sesion:"Autorizado",
-                        hora:"14:00"
-                    });
-                })
-                .catch(error=>{
-                    console.log(error);
-                    res.send(error);
-                })
-            }else{
-                res.render('inicioSesion.html',{
-                    error: 1
-                });
-            }
-        }else{
-            res.render('inicioSesion.html',{
-                error: 2
-            });
-        }        
-    })
-    .catch(error=>{
-        console.log(error);
-        res.send(error);
-    })
-};*/
-
-/*exports.getRegistro = (req,res) =>{
-    console.log(req.params);
-    Usuario.findByPk(req.params.usuario)
-    .then(resultado=>{
-        var data=[];
-        data.push(resultado.dataValues);
-        res.render('DataTable.html',{
-            personas:data,
-            sesion:"Autorizado",
-            hora:"14:00"
-        });
-    })
-    .catch(error=>{
-        console.log(error);
-        res.send(error);
-    })
-}*/
-
-/*exports.postUsuarioTermino = (req,res)=>{
-    console.log(req.body);
-    Usuario.findByPk(req.body.nombreUsuario)
-      .then(usuario=>{    
-      usuario.estadoJuego = true
-      return usuario && usuario.save();
-      });
-    res.redirect("/usuario/confirmacion");
-  };*/
